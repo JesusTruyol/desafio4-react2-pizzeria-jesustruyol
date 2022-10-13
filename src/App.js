6625b5dd6a228077c41a57fb92b535f0def9bba3
JesusTruyol <jesus.truyol@gmail.com>
2022-10-13 19:12:12 -0300

    Desafio utilizando todo lo aprendido en React con useState, useEffect, useNavigate, useContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ContextApi from './context/ContextApi';
+import {useState, useEffect} from 'react';
+import Navbar from './components/Navbar'
+import Home from './views/Home';
+import Description from './views/Description';
+import ShoppingBasket from './views/ShoppingBasket';
+import NotFound from './views/NotFound';
+
 
 function App() {
+
+  const [listProduct, setListProduct]= useState([]);
+  const [totalPrice, setTotalPrice]= useState(0)
+  const url ='/pizzas.json';
+  const globalData= {listProduct, totalPrice, setTotalPrice}
+
+
+  const getDataApi= async ()=>{
+    const response= await fetch(window.location.origin+url);
+    const getdata= await response.json();
+    // Se agrega quantity a cada index del objeto
+    const newData=getdata?.map((data)=> Object.assign(data,{quantity:0}));
+    setListProduct(newData)
+  }
+
+  useEffect(()=>{
+    getDataApi();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <ContextApi.Provider value={globalData}>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/pizza/:id' element={<Description/>}/>
+            <Route path='/carrito' element={<ShoppingBasket/>}/>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>
+        </ContextApi.Provider>
+      </BrowserRouter>
+      
     </div>
   );
 }
